b4de26fb5d24dbcdc63b7e434b51fb4d1571dcd1
Drew Morris <dhmorris@gmail.com>
2013-11-02 22:36:35 -0400

    adding example

---
+++ node/config.example.js
@@ -0,0 +1,5 @@
+var config = {
+	db: 'mongodb://user:pass@widmore.mongohq.com:10000/cohistoria'
+};
+
+module.exports = config;
--- node/config.js
+++
@@ -1,5 +0,0 @@
-var config = {};
-
-config.db = 'mongodb://drewry:hg1m0Bql@widmore.mongohq.com:10000/cohistoria';
-
-module.exports = config;
